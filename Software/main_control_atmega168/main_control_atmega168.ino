256b8402b7f8a35c3029e779c7046efbfb5d5d1f
misa <sampae2402@hotmail.com>
2022-11-19 23:23:50 +0100

    Added He-Encoder and Oxygen sensor, introduced Machine States

--- Software/main_control_atmega168/main_control_atmega168.ino
+++ Software/main_control_atmega168/main_control_atmega168.ino
@@ -34,22 +34,28 @@ LedControl lc = LedControl(DISPLAY_DIN_PIN,DISPLAY_CLK_PIN,DISPLAY_CS_PIN,1);
 #define OXY_ENCODER_DT 4
 #define OXY_ENCODER_SW 2
 
-  long TimeOfLastDebounce = 0;
-  float DelayOfDebounce = 0.1;
-
-
-//Encoder heEncoder(heEncoderDt, heEncoderClk);
-//Encoder oxyEncoder(oxyEncoderDt, oxyEncoderClk);
+long TimeOfLastDebounce= 0;
+float DelayOfDebounce = 0.01;
 
 //* Global variables
-struct {
-  float oxySetPoint = 21;
-} Globals;
+float oxySetPoint = 22;
+int oldClockOxy = -1; 
+bool oxyFixed = false;
 
-enum {
+float heSetPoint = 1;
+int oldClockHe = -1;
+bool heFixed = false;
+
+float oxySensorCalib = 0;
+float oxySensorValue = 21;
+float lastOxyValue = 21;
+
+enum MachineState{
   MixSetting,
   MixingActive,
-} MachineStates;
+};
+
+MachineState currentState = MachineState::MixSetting;
 
 ///////////////////////////////////////////////////////
 // Auxiliary functions
@@ -61,11 +67,36 @@ void setHeO2Display(int o2Soll, int heSoll, int o2Is, int heIs) {
   /* and clear the display */
   lc.clearDisplay(0);
   //todo: error checks
+  lc.setDigit(0,3,(o2Soll/10) % 10,false);
+  lc.setDigit(0,2,(o2Soll) % 10, false);
+
+  lc.setDigit(0,1,(heSoll/10) % 10,false);
+  lc.setDigit(0,0,(heSoll) % 10, false);
+
+  lc.setChar(0,7,'5',false);
+  lc.setChar(0,6,'E',false);
+  
+  //letter 't'
+  lc.setLed(0,5,4,true);  
+  lc.setLed(0,5,5,true);  
+  lc.setLed(0,5,6,true);  
+  lc.setLed(0,5,7,true); 
+  
+  lc.setChar(0,4,' ',false);
+}
+
+void mixHeO2Display(int o2Soll, int heSoll, int o2Is, int heIs) {
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,2);
+  /* and clear the display */
+  lc.clearDisplay(0);
+  //todo: error checks
   lc.setDigit(0,7,(o2Soll/10) % 10,false);
-  lc.setDigit(0,6,(o2Soll) % 10,false);
+  lc.setDigit(0,6,(o2Soll) % 10, false);
 
   lc.setDigit(0,5,(heSoll/10) % 10,false);
-  lc.setDigit(0,4,(heSoll) % 10,false);
+  lc.setDigit(0,4,(heSoll) % 10, false);
 
   lc.setDigit(0,3,(o2Is/10) % 10,false);
   lc.setDigit(0,2,(o2Is) % 10,false);
@@ -74,42 +105,72 @@ void setHeO2Display(int o2Soll, int heSoll, int o2Is, int heIs) {
   lc.setDigit(0,0,(heIs) % 10,false);
 }
 
-void check_rotary() {
-          static int oldClock = -1; // Initialize to an impossible value.
-          int clockVal = digitalRead(OXY_ENCODER_CLK);
-          int dataVal = digitalRead(OXY_ENCODER_DT);
-          if(clockVal == oldClock) return; // was a bounce. Don't count this.
-          if(clockVal ^ dataVal) {
-                  // clockwise move
-                  Globals.oxySetPoint+=0.5;
-          } else {
-               // counterclockwise move
-              Globals.oxySetPoint-=0.5;
-           }
-           if (Globals.oxySetPoint < 0) {
-             Globals.oxySetPoint += 100;
-           }
-            oldClock = clockVal; // store clock state for debounce check.
-          setHeO2Display(Globals.oxySetPoint,0,21,0); 
+void encoder_read(int clk_pin, int dt_pin, float & setpoint, int &oldClock) {
+  int clockVal = digitalRead(clk_pin);
+  int dataVal = digitalRead(dt_pin);
+  if (clockVal == oldClock) return;  // was a bounce. Don't count this.
+  if (clockVal ^ dataVal) {
+    // clockwise move
+    setpoint += 0.5;
+  } else {
+    // counterclockwise move
+    setpoint -= 0.5;
+  }
+  if (setpoint < 0) {
+    setpoint += 100;
+  }
+  oldClock = clockVal;  // store clock state for debounce check.
+  setHeO2Display(oxySetPoint, heSetPoint, 21, 0); //update display
+}
+
+float getAverageSensorVal(int sensor_pin, int num) {
+  float output = 0;
+  for(int i = 0; i < num; ++i) {
+    output += analogRead(sensor_pin);
+  }
+  return output /(float)num;
 }
 
 //////////////////////////////////////////////////////////////////
-// Basic uc methods
+// Basic microcontroller methods
 
 void setup() {  
   Serial.begin(9600); 
+  pinMode(OXY_SENSOR_PIN, INPUT);
+
+  oxySensorCalib = 21.0/getAverageSensorVal(OXY_SENSOR_PIN, 30);
+
+  pinMode(HE_ENCODER_SW, INPUT_PULLUP);
+  pinMode(OXY_ENCODER_SW, INPUT_PULLUP);
   
   lc.shutdown(0,false);
   /* Set the brightness to a medium values */
   lc.setIntensity(0,2);
   /* and clear the display */
   lc.clearDisplay(0);
-  setHeO2Display(21,0,21,0);
+  setHeO2Display(oxySetPoint, heSetPoint, round(oxySensorValue), 0); //update display
 }
 
 void loop() {
-  if((millis() - TimeOfLastDebounce) > DelayOfDebounce){
-    check_rotary();
-    TimeOfLastDebounce = millis();
+  switch(currentState) {
+    case MixSetting:
+      if((millis() - TimeOfLastDebounce) > DelayOfDebounce){
+        encoder_read(HE_ENCODER_CLK, HE_ENCODER_DT, heSetPoint, oldClockHe);
+        encoder_read(OXY_ENCODER_CLK, OXY_ENCODER_DT, oxySetPoint, oldClockOxy);
+        TimeOfLastDebounce = millis();
+      }
+      break;
+    case MixingActive:
+      oxySensorValue = getAverageSensorVal(OXY_SENSOR_PIN, 20)*oxySensorCalib;
+      if (oxySensorValue - lastOxyValue > 1 || oxySensorValue - lastOxyValue < -1) {
+        Serial.println(oxySensorValue);
+        mixHeO2Display(oxySetPoint, heSetPoint, round(oxySensorValue), 0); //update display
+        lastOxyValue = oxySensorValue;
+      }
+      break;
+    default:
+      Serial.println("Error");
   }
+
+
 }
