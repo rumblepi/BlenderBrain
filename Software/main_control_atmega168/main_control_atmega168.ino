3065d229f135bdc5c5d676ef6d4d160736c88d66
Michael <michael.sandbichler@hotmail.com>
2023-03-28 22:41:58 +0200

    added clear method to main control

--- Software/main_control_atmega168/main_control_atmega168.ino
+++ Software/main_control_atmega168/main_control_atmega168.ino
@@ -293,6 +293,10 @@ struct EightDigitDisplay {
       lc_.clearDisplay(0);      
   }
 
+  void clear() {
+    lc_.clearDisplay(0);
+  }  
+
   void writeNumber(int number) {
     // Writes the first eight digits of a given number to the display
     for(int i=0; i<8; ++i) {
@@ -316,6 +320,21 @@ struct EightDigitDisplay {
     }
   }
 
+  void writeFullText(char* txt) {
+    //The length of txt must not be larger than eight characters and will be displayed
+    for(int i = 0; i < 8; ++i) {
+      lc_.setChar(0,7-i,txt[i],false);
+      if(txt[i]=='t'){
+        lc_.setLed(0,7-i,4,true);  
+        lc_.setLed(0,7-i,5,true);  
+        lc_.setLed(0,7-i,6,true);  
+        lc_.setLed(0,7-i,7,true);       
+        }
+      if(txt[i]=='S'){
+        lc_.setDigit(0,7-i,5,false);
+      }
+    }
+  }
   void writeTwoDigits(int number, int pos) {
     //write two digits at position 1,2,3 or 4 of the display |11|22|33|44|
     lc_.setDigit(0,7-2*(pos-1),(number/10) % 10,false);
@@ -505,6 +524,7 @@ void debugDisplay() {
     //} 
 }
 #endif
+
 //////////////////////////////////////////////////////////////////
 // Basic microcontroller methods
 
